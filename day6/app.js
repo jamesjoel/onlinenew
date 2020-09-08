a7066dfeed28bb7fcac8d33749fb3776e6ebe2bd
jamesjoel <james.joelj@gmail.com>
2020-09-08 19:30:43 +0530

    insert in mongodb

---
+++ day6/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/assets"));
+app.use(bodyParser());
+
+
+app.use(require("./config/routes"));
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server running");
+})
---
+++ day6/controllers/student.js
@@ -0,0 +1,98 @@
+var express = require("express");
+var routes = express.Router();
+
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017";
+
+routes.get("/info", (req, res)=>{
+    res.send("this is student->info page");
+});
+routes.get("/demo", (req, res) => {
+    res.send("this is student->demo page");
+});
+
+
+routes.post("/", (req, res)=>{
+
+    console.log(req.body);
+    MongoClient.connect(url, (err, con)=>{
+        var db = con.db("newbatch");
+        db.collection("student").insert(req.body, (err, result)=>{
+            console.log(result);
+            res.redirect("/student");
+        });
+        
+    });
+/*
+    res.send()
+    res.render()
+    res.sendFile()
+    res.redirect()
+
+
+*/
+
+
+});
+
+
+
+routes.get("/", (req, res) => {
+    
+    MongoClient.connect(url, (err, con) => { // connect to DB with URL
+        if (err) {
+            console.log("database error", err);
+            return;
+        }
+        /*  
+            use newbtach
+            db.student.find()
+
+        */
+        var db = con.db("newbatch"); // use or select DB
+        db.collection("student").find().toArray((err, result) => { // db.coll.find(select * from).toArray()
+            if(err){
+                console.log("table error",err);
+                return;
+            }
+            var pageData = { title: "Student", pagename: "student/index", student: result };
+            res.render("layout", pageData);
+        });
+    });
+
+   
+});
+
+module.exports = routes;
+
+
+
+/*
+    MongoClient.connect(url, (err, con)=>{
+
+        var db = con.db("dbname");
+
+        db.collection("coll_name").find().toArray((err, result)=>{
+
+        });
+        db.collection("coll_name").insert({object}, (err, result)=>{
+
+        });
+        db.collection("coll_name").update({object},{where condi}, (err, result)=>{
+
+        });
+        db.collection("coll_name").remove({where condi}, (err, result)=>{
+
+        });
+
+    })
+
+
+
+
+
+
+
+
+
+*/
