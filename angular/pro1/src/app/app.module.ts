ac4510e9998ea73ef62c04c5dbc895bab71a198e
jamesjoel <james.joelj@gmail.com>
2020-09-29 19:12:55 +0530

    component day 1

--- angular/pro1/src/app/app.module.ts
+++ angular/pro1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { Box2Component } from './box2/box2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    Box2Component
   ],
   imports: [
     BrowserModule,
---
+++ angular/pro1/src/app/box2/box2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'indore',
+  templateUrl: './box2.component.html',
+  styleUrls: ['./box2.component.scss']
+})
+export class Box2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
