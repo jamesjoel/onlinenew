c5c5629bdebeefbf0bd17ff5a9645a45eb1fcb2b
jamesjoel <james.joelj@gmail.com>
2020-10-01 19:10:11 +0530

    interplation and *ngFor

--- angular/pro2/src/app/pages/about/about.component.ts
+++ angular/pro2/src/app/pages/about/about.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  name = "rohit";
+  arr = ["red", "green", "blue", "yellow", "black", "white", "pink", "gray"];
+
+  obj = {
+    first : "indore",
+    second : "mumbai",
+    third : "pune"
+  };
+
+  a = 100;
+  b = 200;
+
+  jsonObj = [
+    {
+      name : "rohit",
+      age : 25,
+      city : "indore"
+    },
+    {
+      name : "james",
+      age : 23,
+      city : "mumbai"
+    },
+    {
+      name : "nidhi",
+      age : 27,
+      city : "pune"
+    },
+    {
+      name : "sunil",
+      age : 20,
+      city : "indore"
+    }
+  ];
+
+
+
+
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+  demo(){
+    alert("hello");
   }
 
 }
--- angular/pro2/src/app/pages/contact/contact.component.ts
+++ angular/pro2/src/app/pages/contact/contact.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  x = 200;
+  y = 200;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  demo(){
+    //alert("Hello");
+    this.x = 300;
+    this.y = 400;
+  }
+  demo2(){
+    this.x = 200;
+    this.y = 200;
   }
 
 }
